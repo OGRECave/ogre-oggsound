14b25e120ad06eef27d5ad4c73f9235815fb808a
stickymango <stickymango@138482c5-828b-4ad4-aaaf-513824ee5848>
2008-09-11 10:58:18 +0000

    Added TestDemo project for testing functionality quickly, Added and setup OgreOggSoundLib project to include TestDemo and automatically copy dll's into directories.

---
+++ TestDemo/src/MainApp.cpp
@@ -0,0 +1,291 @@
+#include "MainApp.h"
+#include "MainOgre.h"
+#include "OgreOggSound.h"
+
+using namespace OgreOggSound;
+
+template<> MainApp* Ogre::Singleton<MainApp>::ms_Singleton = 0;
+
+const float CAMERA_ROTATE_SPEED = 0.15f;
+const float CAMERA_TRANSLATE_SPEED = 30.0f;
+//-----------------------------------------------------------------------
+MainApp* MainApp::getSingletonPtr()
+{
+    return ms_Singleton;
+}
+//-----------------------------------------------------------------------
+MainApp& MainApp::getSingleton()
+{  
+    assert( ms_Singleton );
+	return ( *ms_Singleton );  
+}
+//-----------------------------------------------------------------------
+MainApp::MainApp() : 
+mQuit(false),mSceneMgr(0),mWindow(0), mInputManager(0),mFrameTime(0),
+mTranslateVectorCamera(Ogre::Vector3::ZERO),mYawAngleCamera(0)
+{
+	Init();
+}
+//-----------------------------------------------------------------------
+MainApp::~MainApp()
+{	
+	delete mSoundManager;
+}
+//-----------------------------------------------------------------------
+void MainApp::requestQuit()
+{
+	mQuit = true;
+}
+//-----------------------------------------------------------------------
+void MainApp::Init()
+{
+	mSceneMgr = MainOgre::getSingletonPtr()->getSceneManager();
+	mWindow = MainOgre::getSingletonPtr()->getWindow();	
+
+	mInputManager = InputManager::getSingletonPtr();
+	mInputManager->addKeyListener(this,"KeyListener");
+	mInputManager->addMouseListener(this,"MouseListener");
+
+	mSoundManager = new OgreOggSoundManager;
+	
+	createCamera();
+	createViewport();
+	createScene();	
+
+	Ogre::Root::getSingleton().addFrameListener(this);
+}
+//-----------------------------------------------------------------------
+void MainApp::createCamera()
+{
+	mCamera = mSceneMgr->createCamera("MainCamera"); 
+	
+	Ogre::SceneNode* camNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("CamNode");
+	Ogre::SceneNode* pitchNode = camNode->createChildSceneNode("PitchNode");
+	pitchNode->attachObject(mCamera);	
+	camNode->setPosition(0,0,0);
+	
+	Ogre::Quaternion camPitchQuat = Ogre::Quaternion(Ogre::Degree(0),Ogre::Vector3(0,1,0));
+	camNode->getChild(0)->setOrientation(camPitchQuat);	
+	
+	mCamera->setProjectionType(Ogre::PT_PERSPECTIVE);	
+	mCamera->setNearClipDistance(1.0f);
+	mCamera->setFarClipDistance(1000.0f);	
+
+	mCamera->setAutoAspectRatio(true);	
+}
+//-----------------------------------------------------------------------
+void MainApp::createViewport()
+{
+	mVp = mWindow->addViewport(mCamera);	
+	mCamera->setAspectRatio( Ogre::Real(mVp->getActualWidth()) / Ogre::Real(mVp->getActualHeight()) );
+	mVp->setBackgroundColour(Ogre::ColourValue(0,0,0,0));	
+}
+//-----------------------------------------------------------------------
+void MainApp::createScene()
+{
+	Ogre::Entity *eOgreHead = mSceneMgr->createEntity("OgreHead","ogrehead.mesh");
+	Ogre::SceneNode *nOgreHeadAxis = mSceneMgr->getRootSceneNode()->createChildSceneNode("OgreHeadAxis");
+	Ogre::SceneNode *nOgreHead = nOgreHeadAxis->createChildSceneNode("OgreHead",Ogre::Vector3(100,0,0));
+	nOgreHead->setScale(0.5f,0.5f,0.5f);
+	nOgreHead->attachObject(eOgreHead);
+
+	Ogre::Entity *eOgreMonster = mSceneMgr->createEntity("OgreMonster","ogre bodyShapednew.mesh");
+	Ogre::SceneNode *nOgreMonsterAxis = mSceneMgr->getRootSceneNode()->createChildSceneNode("OgreMonsterAxis");
+	Ogre::SceneNode *nOgreMonster = nOgreMonsterAxis->createChildSceneNode("OgreMonster");
+	nOgreMonster->setScale(0.5f,0.5f,0.5f);
+	nOgreMonster->attachObject(eOgreMonster);
+	nOgreMonster->setPosition(0,10,-300);
+
+	Ogre::Light *light = mSceneMgr->createLight("MainLight");
+	light->setType(Ogre::Light::LT_POINT);
+	light->setPosition(0,200,-250);
+	light->setSpecularColour(1.0f,1.0f,1.0f);
+
+
+	mSoundManager->init("Generic Software");
+	mSoundManager->setDistanceModel(AL_LINEAR_DISTANCE);
+
+	mCamera->getParentSceneNode()->attachObject(mSoundManager->getListener());
+
+	/** Sound one - non streamed, looping, moving */
+	mSoundManager->createSound("One", "one.ogg", false, true);	
+	mSoundManager->getSound("One")->setMaxDistance(200);
+	mSoundManager->getSound("One")->setReferenceDistance(50);
+	mSoundManager->getSound("One")->play();
+	nOgreHead->attachObject(mSoundManager->getSound("One"));
+	
+
+	/** Sound two - prebuffered, streamed, looping, EFX room effect */
+	EAXREVERBPROPERTIES props = REVERB_PRESET_AUDITORIUM;
+	mSoundManager->createSound("Two", "two.ogg", true, true, true);	
+	mSoundManager->getSound("Two")->setMaxDistance(400);
+	mSoundManager->getSound("Two")->setReferenceDistance(30);
+	if ( mSoundManager->hasEFXSupport() )
+	{
+		mSoundManager->createEFXSlot();
+		mSoundManager->createEFXEffect("Auditorium", AL_EFFECT_EAXREVERB, &props);
+		mSoundManager->attachEffectToSound("Two", 0, "Auditorium");
+	}
+	mSoundManager->getSound("Two")->play();
+	
+	/** Sound one - streamed, looping, EFX Direct filter */
+	mSoundManager->createSound("background", "background.ogg", true, true, true);
+	mSoundManager->getSound("background")->setRelativeToListener(true);
+	mSoundManager->getSound("background")->setVolume(0.2f);
+	if ( mSoundManager->hasEFXSupport() )
+	{
+		mSoundManager->createEFXFilter("LowPassTest", AL_FILTER_LOWPASS, 0.1, 0.5);
+		mSoundManager->attachFilterToSound("background", "LowPassTest");
+	}
+	mSoundManager->getSound("background")->play();
+}
+//-----------------------------------------------------------------------
+void MainApp::finishedCB(OgreOggISound* sound)
+{
+	Ogre::String msg="*** sound: "+sound->getName()+" has Looped!";
+	Ogre::LogManager::getSingleton().logMessage(msg);
+}
+//-----------------------------------------------------------------------
+bool MainApp::frameStarted( const Ogre::FrameEvent& evt )
+{
+	mFrameTime = evt.timeSinceLastFrame;
+
+	mInputManager->capture();	
+
+	if (mTranslateVectorCamera != Ogre::Vector3::ZERO)
+	{
+		mCamera->getParentSceneNode()->translate(mCamera->getDerivedOrientation() * mTranslateVectorCamera * mFrameTime);
+		mCamera->getParentSceneNode()->yaw(Ogre::Degree(mYawAngleCamera * mFrameTime));
+	}	
+
+	Ogre::SceneNode *nHeadAxis = mSceneMgr->getSceneNode("OgreHeadAxis");
+	nHeadAxis->rotate(Ogre::Quaternion(Ogre::Degree(20.0f * evt.timeSinceLastFrame),Ogre::Vector3::UNIT_Y));
+
+	Ogre::SceneNode *nMonsterAxis = mSceneMgr->getSceneNode("OgreMonsterAxis");
+
+	mSoundManager->update();
+	
+	if (mQuit) return false;	
+
+	return true;
+}
+//-----------------------------------------------------------------------
+bool MainApp::frameEnded( const Ogre::FrameEvent& evt )
+{
+	if (mWindow->isClosed())	
+		return false;		
+	
+	updateStats();
+	
+	return true;
+}
+//-----------------------------------------------------------------------
+void MainApp::updateStats()
+{
+		static Ogre::String currFps = "Current FPS: ";
+		static Ogre::String avgFps = "Average FPS: ";
+		static Ogre::String bestFps = "Best FPS: ";
+		static Ogre::String worstFps = "Worst FPS: ";
+		static Ogre::String tris = "Triangle Count: ";
+		static Ogre::String batches = "Batch Count: ";
+
+		// update stats when necessary
+		try {
+			Ogre::OverlayElement* guiAvg = Ogre::OverlayManager::getSingleton().getOverlayElement("Core/AverageFps");
+			Ogre::OverlayElement* guiCurr = Ogre::OverlayManager::getSingleton().getOverlayElement("Core/CurrFps");
+			Ogre::OverlayElement* guiBest = Ogre::OverlayManager::getSingleton().getOverlayElement("Core/BestFps");
+			Ogre::OverlayElement* guiWorst = Ogre::OverlayManager::getSingleton().getOverlayElement("Core/WorstFps");
+
+			const Ogre::RenderTarget::FrameStats& stats = mWindow->getStatistics();
+			guiAvg->setCaption(avgFps + Ogre::StringConverter::toString(stats.avgFPS));
+			guiCurr->setCaption(currFps + Ogre::StringConverter::toString(stats.lastFPS));
+			guiBest->setCaption(bestFps + Ogre::StringConverter::toString(stats.bestFPS)
+				+" "+Ogre::StringConverter::toString(stats.bestFrameTime)+" ms");
+			guiWorst->setCaption(worstFps + Ogre::StringConverter::toString(stats.worstFPS)
+				+" "+Ogre::StringConverter::toString(stats.worstFrameTime)+" ms");
+
+			Ogre::OverlayElement* guiTris = Ogre::OverlayManager::getSingleton().getOverlayElement("Core/NumTris");
+			guiTris->setCaption(tris + Ogre::StringConverter::toString(stats.triangleCount));
+
+			Ogre::OverlayElement* guiBatches = Ogre::OverlayManager::getSingleton().getOverlayElement("Core/NumBatches");
+			guiBatches->setCaption(batches + Ogre::StringConverter::toString(stats.batchCount));
+		}
+		catch(...) { /* ignore */ }
+}
+//-----------------------------------------------------------------------
+bool MainApp::keyPressed( const OIS::KeyEvent &arg )
+{	
+	// Quit the application
+	if (arg.key == OIS::KC_ESCAPE)   
+		mQuit = true;
+
+	if (arg.key == OIS::KC_F1)
+	{
+		if(mSoundManager->getSound("One")->isPlaying())
+			mSoundManager->getSound("One")->stop();
+		else mSoundManager->getSound("One")->play();
+	}
+
+	if (arg.key == OIS::KC_F2)
+	{
+		if(mSoundManager->getSound("Two")->isPlaying())
+			mSoundManager->getSound("Two")->stop();
+		else mSoundManager->getSound("Two")->play();
+	}
+
+	if (arg.key == OIS::KC_F3)
+	{
+		if(mSoundManager->getSound("background")->isPlaying())
+			mSoundManager->getSound("background")->pause();
+		else mSoundManager->getSound("background")->play();
+	}
+
+	// Show the statistics
+	if( arg.key == OIS::KC_H )
+	{
+		Ogre::Overlay *mDebugPanelOverlay = Ogre::OverlayManager::getSingleton().getByName("Core/DebugOverlay");
+		mDebugPanelOverlay->isVisible() ? mDebugPanelOverlay->hide() :  mDebugPanelOverlay->show();
+	}
+
+	return true;
+}
+//-----------------------------------------------------------------------
+bool MainApp::keyReleased( const OIS::KeyEvent &arg )
+{	
+	
+	return true;
+}
+//-----------------------------------------------------------------------
+bool MainApp::mouseMoved(const OIS::MouseEvent &arg)
+{	
+	Ogre::Real mouseAbsX = arg.state.X.abs;
+	Ogre::Real cenX = MainOgre::getSingleton().getWindow()->getWidth() / 2.0f;	
+	mYawAngleCamera = (mouseAbsX - cenX) * -CAMERA_ROTATE_SPEED;
+
+	return true;
+}
+//-----------------------------------------------------------------------
+bool MainApp::mousePressed(const OIS::MouseEvent &arg ,OIS::MouseButtonID id)
+{			
+	if (id == OIS::MB_Left)
+		mTranslateVectorCamera = Ogre::Vector3(0.0, 0.0, -1.0) * CAMERA_TRANSLATE_SPEED;
+	if (id == OIS::MB_Right)
+		mTranslateVectorCamera = Ogre::Vector3(0.0, 0.0, 1.0) * CAMERA_TRANSLATE_SPEED;
+	
+	return true;
+}
+//-----------------------------------------------------------------------
+bool MainApp::mouseReleased(const OIS::MouseEvent &arg ,OIS::MouseButtonID id)
+{		
+	mTranslateVectorCamera = Ogre::Vector3::ZERO;
+	return true;
+}
+//-----------------------------------------------------------------------
+void MainApp::resize()
+{
+	if (mWindow)
+	{
+		mWindow->windowMovedOrResized();												
+		InputManager::getSingletonPtr()->setWindowExtents(mVp->getActualWidth(),mVp->getActualHeight());		
+	}
+}
---
+++ TestDemo/src/main.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include "Ogre.h"
+#include "MainOgre.h"
+#include "InputManager.h"
+#include "MainApp.h"
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
+
+
+#ifdef _STEREO
+#include "StereoCamera.h"
+#endif
+
+void showWin32Console();
+
+MainOgre *og = NULL;
+
+//-----------------------------------------------------------------------
+LRESULT CALLBACK MsgProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
+{
+	switch (msg)
+	{
+		HDC   hdc;
+		PAINTSTRUCT ps;			
+
+		case WM_SIZE:			
+			if (og)
+			{			
+				MainApp::getSingleton().resize();				
+				break;
+			}		
+
+		case WM_PAINT:				
+			hdc = BeginPaint(hWnd,&ps); 			
+			EndPaint(hWnd,&ps); 
+			break;				
+
+		case WM_DESTROY:		
+			PostQuitMessage(0);
+			return 0;					
+
+		default:
+			break;
+	} 
+
+	return DefWindowProc( hWnd, msg, wParam, lParam );
+}
+//-----------------------------------------------------------------------
+INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR, INT )
+{
+	// Register the mWindow class
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, 
+		GetModuleHandle(NULL), NULL, LoadCursor(NULL,IDC_ARROW), NULL, NULL,
+		"Ogre Win32", NULL };
+	 
+	RegisterClassEx( &wc );
+
+
+// Create the application's window full screen STEREO      
+#ifdef _STEREO
+	HWND hWnd = CreateWindow( "Ogre Win32", "OgreAudiere", 
+                               WS_POPUP|WS_VISIBLE  , 0, 0, 1360, 768,
+                              0, NULL, wc.hInstance, NULL );  
+#else
+// Create the application's window full screen MONO
+//in a window
+	HWND hWnd = CreateWindow( "Ogre Win32", "Sample 1", 
+                              WS_OVERLAPPEDWINDOW  , 0, 0, 800, 600,
+                              0, NULL, wc.hInstance, NULL );
+//full screen
+	// Create the application's window full screen MONO
+/*	HWND hWnd = CreateWindow( "Ogre Win32", "OgreAudiere", 
+                              WS_POPUP|WS_VISIBLE  , 0, 0, 1366, 768,
+                              0, NULL, wc.hInstance, NULL );*/
+#endif
+
+	showWin32Console();	
+
+	//og = MainOgre::createOgre(hWnd); 	
+	og = new MainOgre(hWnd);
+
+	og->Init();
+
+	MainApp *app = new MainApp;	
+
+	ShowWindow( hWnd, SW_SHOWDEFAULT );		
+	UpdateWindow( hWnd );
+
+	MSG msg;
+	ZeroMemory(&msg,sizeof(msg)); 
+	while(msg.message != WM_QUIT)
+	{	
+		if (PeekMessage( &msg,NULL,0,0,PM_REMOVE ))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		
+		else 
+		{			
+			if (!og->Render())
+				break;
+		}		
+	}
+	
+	delete app;	
+	delete og;
+
+	ChangeDisplaySettings(0, 0);
+
+	return 0;
+}
+//-----------------------------------------------------------------------
+void showWin32Console()
+{
+	static const WORD MAX_CONSOLE_LINES = 5000;
+	int hConHandle;
+	long lStdHandle;
+	CONSOLE_SCREEN_BUFFER_INFO coninfo;
+	FILE *fp;
+	// allocate a console for this app
+	AllocConsole();
+	// set the screen buffer to be big enough to let us scroll text
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
+	coninfo.dwSize.Y = MAX_CONSOLE_LINES;
+	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE),
+	coninfo.dwSize);
+	// redirect unbuffered STDOUT to the console
+	lStdHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
+	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
+	fp = _fdopen( hConHandle, "w" );
+	*stdout = *fp;
+	setvbuf( stdout, NULL, _IONBF, 0 );
+	// redirect unbuffered STDIN to the console
+	lStdHandle = (long)GetStdHandle(STD_INPUT_HANDLE);
+	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
+	fp = _fdopen( hConHandle, "r" );
+	*stdin = *fp;
+	setvbuf( stdin, NULL, _IONBF, 0 );
+	// redirect unbuffered STDERR to the console
+	lStdHandle = (long)GetStdHandle(STD_ERROR_HANDLE);
+	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
+	fp = _fdopen( hConHandle, "w" );
+	*stderr = *fp;
+	setvbuf( stderr, NULL, _IONBF, 0 );
+	// make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog
+	// point to console as well
+	std::ios::sync_with_stdio();
+}
