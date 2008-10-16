2c42ae05b3f54e367be504968b6aa13c1eec9cf7
stickymango <stickymango@138482c5-828b-4ad4-aaaf-513824ee5848>
2008-10-16 09:14:02 +0000

    Removed seek/tell callbacks for static ogg files, should speed up loading significantly.

--- trunk/TestDemo/src/MainApp.cpp
+++ trunk/TestDemo/src/MainApp.cpp
@@ -115,7 +115,7 @@ void MainApp::createScene()
 	mCamera->getParentSceneNode()->attachObject(mSoundManager->getListener());
 
 	/** Sound one - non streamed, looping, moving */
-	mSoundManager->createSound("One", "one.ogg", true, true);	
+	mSoundManager->createSound("One", "one.ogg", false, true);	
 	mSoundManager->getSound("One")->setMaxDistance(250);
 	mSoundManager->getSound("One")->setReferenceDistance(50);
 	nOgreHead->attachObject(mSoundManager->getSound("One"));
