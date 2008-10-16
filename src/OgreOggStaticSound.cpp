15f085e6960c13710addc8d43aab43d85eaa3d0d
stickymango <stickymango@138482c5-828b-4ad4-aaaf-513824ee5848>
2008-10-16 09:14:02 +0000

    Removed seek/tell callbacks for static ogg files, should speed up loading significantly.

--- src/OgreOggStaticSound.cpp
+++ src/OgreOggStaticSound.cpp
@@ -28,60 +28,6 @@ using namespace std;
 namespace OgreOggSound
 {
 
-	void Load_Wave_File(const char *fname)
-	{
-		FILE *fp;
-
-		fp = fopen(fname,"rb");
-		if (fp)
-		{
-			char id[4], *sound_buffer; //four bytes to hold 'RIFF'
-
-			DWORD size; //32 bit value to hold file size
-
-			short	format_tag, 
-				channels, 
-				block_align, 
-				bits_per_sample; //our 16 values
-
-			DWORD	format_length, 
-				sample_rate, 
-				avg_bytes_sec, 
-				data_size;
-
-			fread(id, sizeof(BYTE), 4, fp); //read in first four bytes
-
-			if (!strcmp(id, "RIFF"))
-			{ 
-				//we had 'RIFF' let's continue
-				fread(&size, sizeof(DWORD), 1, fp); //read in 32bit size value
-				fread(&id, sizeof(BYTE), 4, fp); //read in 4 byte string now
-
-				if (!strcmp(id,"WAVE"))
-				{ 
-					//this is probably a wave file since it contained "WAVE"
-					fread(id, sizeof(BYTE), 4, fp); //read in 4 bytes "fmt ";
-					fread(&format_length, sizeof(DWORD),1,fp);
-					fread(&format_tag, sizeof(short), 1, fp); //check mmreg.h (i think?) for other 
-					// possible format tags like ADPCM
-					fread(&channels, sizeof(short),1,fp); //1 mono, 2 stereo
-					fread(&sample_rate, sizeof(DWORD), 1, fp); //like 44100, 22050, etc...
-					fread(&avg_bytes_sec, sizeof(short), 1, fp); //probably won't need this
-					fread(&block_align, sizeof(short), 1, fp); //probably won't need this
-					fread(&bits_per_sample, sizeof(short), 1, fp); //8 bit or 16 bit file?
-					fread(id, sizeof(BYTE), 4, fp); //read in 'data'
-					fread(&data_size, sizeof(DWORD), 1, fp); //how many bytes of sound data we have
-					sound_buffer = new char[data_size]; //set aside sound buffer space
-					fread(sound_buffer, sizeof(BYTE), data_size, fp); //read in our whole sound data chunk
-				}
-				else
-					printf("Error: RIFF file but not a wave file\n");
-			}
-			else
-				printf("Error: not a RIFF file\n");
-		}
-	}
-
 	/*/////////////////////////////////////////////////////////////////*/
 	OgreOggStaticSound::OgreOggStaticSound(const Ogre::String& name) : OgreOggISound(name)
 	{
@@ -91,7 +37,10 @@ namespace OgreOggSound
 		mVorbisComment=0;		
 		mBufferData.clear();	
 		mPreviousOffset=0;
-		mBuffer=0;						
+		mBuffer=0;			
+		// Disable seeking
+		mOggCallbacks.seek_func=NULL;
+		mOggCallbacks.tell_func=NULL;
 	}
 	/*/////////////////////////////////////////////////////////////////*/
 	OgreOggStaticSound::~OgreOggStaticSound()
