133b5e72b6ac8aae192ad1b2e5393b5bf4ca4443
stickymango <stickymango@138482c5-828b-4ad4-aaaf-513824ee5848>
2009-02-16 23:46:12 +0000

    Cmake build

--- include/OgreOggSoundPrereqs.h
+++ include/OgreOggSoundPrereqs.h
@@ -1,26 +1,26 @@
 /*---------------------------------------------------------------------------*\
-** This source file is part of OgreOggSound, an OpenAL wrapper library for   
-** use with the Ogre Rendering Engine.										 
-**                                                                           
-** Copyright 2008 Ian Stangoe & Eric Boissard								 
-**                                                                           
-** OgreOggSound is free software: you can redistribute it and/or modify		  
-** it under the terms of the GNU Lesser General Public License as published	 
-** by the Free Software Foundation, either version 3 of the License, or		 
-** (at your option) any later version.										 
-**																			 
-** OgreOggSound is distributed in the hope that it will be useful,			 
-** but WITHOUT ANY WARRANTY; without even the implied warranty of			 
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			 
-** GNU Lesser General Public License for more details.						 
-**																			 
-** You should have received a copy of the GNU Lesser General Public License	 
-** along with OgreOggSound.  If not, see <http://www.gnu.org/licenses/>.	 
+** This source file is part of OgreOggSound, an OpenAL wrapper library for
+** use with the Ogre Rendering Engine.
+**
+** Copyright 2008 Ian Stangoe & Eric Boissard
+**
+** OgreOggSound is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published
+** by the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** OgreOggSound is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with OgreOggSound.  If not, see <http://www.gnu.org/licenses/>.
 \*---------------------------------------------------------------------------*/
 
 #pragma once
 
-#include "Ogre.h"
+#include "OGRE/Ogre.h"
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #	include "al.h"
@@ -36,7 +36,7 @@
 #			define _OGGSOUND_EXPORT __declspec(dllimport)
 #		endif
 #	else
-#		define _OGGSOUND_EXPORT 
+#		define _OGGSOUND_EXPORT
 #	endif
 #elif OGRE_COMPILER == OGRE_COMPILER_GNUC
 #	include <AL/al.h>
@@ -55,8 +55,8 @@
 
 /**
  * Specifies whether to use threads for streaming
- * 0 - No multithreading 
- * 1 - BOOST multithreaded 
+ * 0 - No multithreading
+ * 1 - BOOST multithreaded
  */
 #ifndef OGGSOUND_THREADED
 	#define OGGSOUND_THREADED 1
