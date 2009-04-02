72e700d057733b1ecf650af88061e134ba877add
stickymango <stickymango@138482c5-828b-4ad4-aaaf-513824ee5848>
2009-04-02 18:29:54 +0000

    added linux cmake files and lib

---
+++ trunk/CMakeFiles/ogreoggsound.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daddy/Documents/OgreOggSound_new/src/OgreOggISound.cpp" "/home/daddy/Documents/OgreOggSound_new/CMakeFiles/ogreoggsound.dir/src/OgreOggISound.cpp.o"
+  "/home/daddy/Documents/OgreOggSound_new/src/OgreOggListener.cpp" "/home/daddy/Documents/OgreOggSound_new/CMakeFiles/ogreoggsound.dir/src/OgreOggListener.cpp.o"
+  "/home/daddy/Documents/OgreOggSound_new/src/OgreOggSoundFactory.cpp" "/home/daddy/Documents/OgreOggSound_new/CMakeFiles/ogreoggsound.dir/src/OgreOggSoundFactory.cpp.o"
+  "/home/daddy/Documents/OgreOggSound_new/src/OgreOggSoundPlugin.cpp" "/home/daddy/Documents/OgreOggSound_new/CMakeFiles/ogreoggsound.dir/src/OgreOggSoundPlugin.cpp.o"
+  "/home/daddy/Documents/OgreOggSound_new/src/OgreOggSoundPluginDllStart.cpp" "/home/daddy/Documents/OgreOggSound_new/CMakeFiles/ogreoggsound.dir/src/OgreOggSoundPluginDllStart.cpp.o"
+  "/home/daddy/Documents/OgreOggSound_new/src/OgreOggSoundRecord.cpp" "/home/daddy/Documents/OgreOggSound_new/CMakeFiles/ogreoggsound.dir/src/OgreOggSoundRecord.cpp.o"
+  "/home/daddy/Documents/OgreOggSound_new/src/OgreOggStaticSound.cpp" "/home/daddy/Documents/OgreOggSound_new/CMakeFiles/ogreoggsound.dir/src/OgreOggStaticSound.cpp.o"
+  "/home/daddy/Documents/OgreOggSound_new/src/OgreOggStaticWavSound.cpp" "/home/daddy/Documents/OgreOggSound_new/CMakeFiles/ogreoggsound.dir/src/OgreOggStaticWavSound.cpp.o"
+  "/home/daddy/Documents/OgreOggSound_new/src/OgreOggStreamWavSound.cpp" "/home/daddy/Documents/OgreOggSound_new/CMakeFiles/ogreoggsound.dir/src/OgreOggStreamWavSound.cpp.o"
+  "/home/daddy/Documents/OgreOggSound_new/src/ogreoggsoundmanager.cpp" "/home/daddy/Documents/OgreOggSound_new/CMakeFiles/ogreoggsound.dir/src/ogreoggsoundmanager.cpp.o"
+  "/home/daddy/Documents/OgreOggSound_new/src/ogreoggstreamsound.cpp" "/home/daddy/Documents/OgreOggSound_new/CMakeFiles/ogreoggsound.dir/src/ogreoggstreamsound.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "LINUX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ trunk/CMakeFiles/ogreoggsound.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ogreoggsound.dir/src/OgreOggSoundRecord.cpp.o"
+  "CMakeFiles/ogreoggsound.dir/src/OgreOggSoundFactory.cpp.o"
+  "CMakeFiles/ogreoggsound.dir/src/OgreOggListener.cpp.o"
+  "CMakeFiles/ogreoggsound.dir/src/OgreOggSoundPlugin.cpp.o"
+  "CMakeFiles/ogreoggsound.dir/src/ogreoggstreamsound.cpp.o"
+  "CMakeFiles/ogreoggsound.dir/src/OgreOggSoundPluginDllStart.cpp.o"
+  "CMakeFiles/ogreoggsound.dir/src/ogreoggsoundmanager.cpp.o"
+  "CMakeFiles/ogreoggsound.dir/src/OgreOggISound.cpp.o"
+  "CMakeFiles/ogreoggsound.dir/src/OgreOggStreamWavSound.cpp.o"
+  "CMakeFiles/ogreoggsound.dir/src/OgreOggStaticWavSound.cpp.o"
+  "CMakeFiles/ogreoggsound.dir/src/OgreOggStaticSound.cpp.o"
+  "libogreoggsound.pdb"
+  "libogreoggsound.a"
+  "libogreoggsound.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ogreoggsound.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
